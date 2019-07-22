f1e6e96d67ea0864dd556c70dd702186df9637db
Luke Aeschleman <lukeaeschleman@gmail.com>
2019-07-21 20:53:27 -0400

    organize

---
+++ scripts/build-pdf.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+
+const order = [
+    'title_page',
+    'table_of_contents',
+    'preface',
+    'introduction',
+    'part_1',
+    'chapter_1'
+]
+
+
+
+
+
+
+
+
+
+
+
